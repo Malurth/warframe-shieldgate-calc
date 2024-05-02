72dd270985a8f71980291a7fb151cfe47a515e82
John Baima <Malurth@users.noreply.github.com>
2024-05-02 00:43:25 -0400

    continued efforts to make UI prettier

--- src/App.svelte
+++ src/App.svelte
@@ -70,10 +70,15 @@
     />
   {/if}
 
-  <p>Shield Gate Duration: {outputTime.toFixed(2)} seconds</p>
+  <p>
+    Shield Gate Duration: <span class="outputNum">{outputTime.toFixed(2)}</span>
+    seconds
+  </p>
 </main>
 
 <style>
+  @import url("https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+
   :global(body) {
     display: flex;
     justify-content: center;
@@ -99,9 +104,15 @@
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   }
 
+  .outputNum {
+    font-size: 24px;
+    font-weight: bold;
+  }
+
   h1 {
     text-align: center;
     margin-bottom: 20px;
+    font-size: 24px;
   }
 
   label {
