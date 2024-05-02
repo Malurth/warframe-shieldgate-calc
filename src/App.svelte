68f97299ef79b3863eb9f2b0c57ceeafd5100e2c
John Baima <Malurth@users.noreply.github.com>
2024-05-01 23:07:22 -0400

    I've been reading Refactoring UI (also I meant checkbox on last commit)

--- src/App.svelte
+++ src/App.svelte
@@ -84,11 +84,17 @@
   }
 
   main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 10px;
     max-width: 400px;
     margin: 0 auto;
     background-color: #222627;
-    padding-right: 20px;
-    padding-left: 20px;
+    padding-right: 40px;
+    padding-left: 40px;
+    padding-top: 20px;
+    padding-bottom: 20px;
     border-radius: 8px;
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   }
